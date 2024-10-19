a295c610d28f262be4b016a78bc8e29bccbaeb0a
NitroKnight68 <nish0602@gmail.com>
2024-10-19 23:32:53 +0530

    feat: book cards

--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -12,26 +12,26 @@ const Book = (props: Props) => {
             <Navbar wallet={props.wallet} />
             <div className="book-h1">EVENTS</div>
             <div className="book-cardlist">
-                <div className="book-card">
+                <div className="book-card card1">
                     <div className="card-img"></div>
+                    <div className="card-sold">SOLD OUT</div>
                     <div className="card-h2">COLDPLAY</div>
-                    <div className="card-date">26th October, 2024</div>
-                    <div className="card-venue">Mumbai</div>
-                    <div className="card-sold">Sold Out</div>
+                    <div className="card-date">26 OCT</div>
+                    <div className="card-venue">MUMBAI</div>
                 </div>
-                <div className="book-card">
+                <div className="book-card card2">
                     <div className="card-img"></div>
+                    <div className="card-sold">SOLD OUT</div>
                     <div className="card-h2">COLDPLAY</div>
-                    <div className="card-date">26th October, 2024</div>
-                    <div className="card-venue">Mumbai</div>
-                    <div className="card-sold">Sold Out</div>
+                    <div className="card-date">26 OCT</div>
+                    <div className="card-venue">MUMBAI</div>
                 </div>
-                <div className="book-card">
+                <div className="book-card card3">
                     <div className="card-img"></div>
+                    <div className="card-sold">SOLD OUT</div>
                     <div className="card-h2">COLDPLAY</div>
-                    <div className="card-date">26th October, 2024</div>
-                    <div className="card-venue">Mumbai</div>
-                    <div className="card-sold">Sold Out</div>
+                    <div className="card-date">26 OCT</div>
+                    <div className="card-venue">MUMBAI</div>
                 </div>
             </div>
         </div>
