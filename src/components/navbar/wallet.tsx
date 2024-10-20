7abb73fd351bfb09dcbbb3677e32f5ec0bc5b214
NitroKnight68 <nish0602@gmail.com>
2024-10-20 06:26:09 +0530

    feat: tickets button

--- src/components/navbar/wallet.tsx
+++ src/components/navbar/wallet.tsx
@@ -1,41 +1,36 @@
 import { useEffect, useState } from "react";
 
-const WalletNav = ({
-  walletType,
-  wallets,
-  connect,
-  disconnect,
-}: walletInterfaceProps) => {
-  const [addr, setAddr] = useState("");
-  useEffect(() => {
-    setAddr(wallets[walletType]);
-    console.log(wallets);
-  }, [wallets, walletType]);
-  return (
-    <>
-      <button
-        className="connect-button"
-        onClick={async () => {
-          setAddr((await connect(walletType)) || "");
-        }}
-      >
-        {addr || `CONNECT WALLET`}
-      </button>
-      <br />
-      {addr ? (
-        <button
-          className="disconnect-button"
-          onClick={async () => {
-            setAddr((await disconnect(walletType)) || "");
-          }}
-        >
-          <i className="fa-solid fa-arrow-right-from-bracket"></i>
-        </button>
-      ) : (
-        <></>
-      )}
-    </>
-  );
+const WalletNav = ({ walletType, wallets, connect, disconnect }: walletInterfaceProps) => {
+    const [addr, setAddr] = useState("");
+    useEffect(() => {
+        setAddr(wallets[walletType]);
+        console.log(wallets);
+    }, [wallets, walletType]);
+    return (
+        <>
+            <button
+                className="connect-button"
+                onClick={async () => {
+                    setAddr((await connect(walletType)) || "");
+                }}
+            >
+                {addr || `CONNECT WALLET`}
+            </button>
+            <br />
+            {addr ? (
+                <button
+                    className="disconnect-button"
+                    onClick={async () => {
+                        setAddr((await disconnect(walletType)) || "");
+                    }}
+                >
+                    <i className="fa-solid fa-arrow-right-from-bracket"></i>
+                </button>
+            ) : (
+                <></>
+            )}
+        </>
+    );
 };
 
 export default WalletNav;
--- src/pages/book/Book.tsx
+++ src/pages/book/Book.tsx
@@ -6,57 +6,57 @@ import buyTicket from "../../helpers/contracts/buyTicket";
 import toast from "react-hot-toast";
 
 interface Props {
-  wallet: walletInterfaceProps;
+    wallet: walletInterfaceProps;
 }
 
 const Book = (props: Props) => {
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  const [events, setEvents] = useState<any[]>([]);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const [events, setEvents] = useState<any[]>([]);
 
-  useEffect(() => {
-    (async () => {
-      const data = await fetchEvents();
-      setEvents(data);
-      console.log(data);
-    })();
-  }, []);
+    useEffect(() => {
+        (async () => {
+            const data = await fetchEvents();
+            setEvents(data);
+            console.log(data);
+        })();
+    }, []);
 
-  return (
-    <div className="book-div">
-      <Navbar wallet={props.wallet} />
-      <div className="book-h1">EVENTS</div>
-      <div className="book-cardlist">
-        {events.map((eve, ind) => (
-          <div className={`book-card card${(ind % 3) + 1}`} key={ind}>
-            <div className="card-img"></div>
-            {/* <div className="card-sold">SOLD OUT</div> */}
-            <div className="card-h2">{eve["parameter"]["value"]["name"]}</div>
-            <div className="card-date" style={{ fontFamily:"sans-serif" }}>
-              {eve["parameter"]["value"]["price"]} Tez
+    return (
+        <div className="book-div">
+            <Navbar wallet={props.wallet} />
+            <div className="book-h1">EVENTS</div>
+            <div className="book-cardlist">
+                {events.map((eve, ind) => (
+                    <div className={`book-card card${(ind % 3) + 1}`} key={ind}>
+                        <div className="card-img"></div>
+                        <div className="card-sold">IN STOCK</div>
+                        <div className="card-h2">{eve["parameter"]["value"]["name"]}</div>
+                        <div className="card-date" style={{ fontFamily: "sans-serif" }}>
+                            {eve["parameter"]["value"]["price"]} XTZ
+                        </div>
+                        <div
+                            className="card-venue"
+                            onClick={async () => {
+                                const rp = await buyTicket(
+                                    props.wallet.dAppclient,
+                                    eve["parameter"]["value"]["name"],
+                                    1,
+                                    eve["parameter"]["value"]["price"]
+                                );
+                                if (rp["status"]) {
+                                    toast.success("Transaction is Succesfull. ");
+                                } else {
+                                    toast.error("Transaction has failed.");
+                                }
+                            }}
+                        >
+                            <p>BUY NOW</p>
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div
-              className="card-venue"
-              onClick={async () => {
-                const rp = await buyTicket(
-                  props.wallet.dAppclient,
-                  eve["parameter"]["value"]["name"],
-                  1,
-                  eve["parameter"]["value"]["price"]
-                );
-                if (rp["status"]) {
-                  toast.success("Transaction is Succesfull. ");
-                } else {
-                  toast.error("Transaction has failed.");
-                }
-              }}
-            >
-              <p>Buy Now</p>
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 };
 
 export default Book;
